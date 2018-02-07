341657a093705a3d28566568dcecca898eb6c4b0
Tapasya Patki <patki1@llnl.gov>
2018-02-07 02:09:08 -0800

    check elapsed time

--- cmd_test.sh
+++ cmd_test.sh
@@ -3,6 +3,6 @@
 #./resource-query -G test/data/grugs/simple_power_node.graphml -S CA -P power < cmd_inp
 
 
-./resource-query -G test/data/grugs/simplest_power_node.graphml -S CA -P power < cmd_inp_simplest
+./resource-query -G test/data/grugs/simplest_power_node.graphml -S CA -P power -e 1 < cmd_inp_simplest
 
 #/Users/patki1/homebrew/brew/bin/gdb ./resource-query -G test/data/grugs/simple_power_node.graphml -S CA -P power < cmd_inp
