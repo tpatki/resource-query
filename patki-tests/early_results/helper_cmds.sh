70002227a4c8acfd81af659245f4511d48727591
Tapasya Patki <patki1@llnl.gov>
2018-03-23 12:01:31 -0700

    Some initial analysis

---
+++ patki-tests/early_results/helper_cmds.sh
@@ -0,0 +1,8 @@
+cat baseline_results.dat | grep ELAPSE | cut -d "=" -f 2 > baseline_sched_time
+cat varaware_results.dat | grep ELAPSE | cut -d "=" -f 2 > varaware_sched_time
+cat baseline_results.dat | grep RESOURCES | cut -d "=" -f 2 > baseline_alloc_vs_reserve
+cat varaware_results.dat | grep RESOURCES | cut -d "=" -f 2 > varaware_alloc_vs_reserve
+
+# The following did not show any difference for this test: same JOB IDs were allocated/reserved.
+# 62 jobs were allocated, rest were reserved for future time.
+diff --side-by-side varaware_alloc_vs_reserve baseline_alloc_vs_reserve
---
+++ patki-tests/early_results/sched_time.R
@@ -0,0 +1,18 @@
+b<-read.table('baseline_sched_time', header = FALSE)
+v<-read.table('varaware_sched_time', header = FALSE)
+
+pdf("sched_time.pdf", width = 8, height = 6)
+par(mar = c(3,4,1,1), mfrow=c(2,1))
+
+plot(b$V1, xlim=c(1,200), ylim = c(0,1.5), ylab = 'Scheduling Time (s)', las = 1, xlab = "", col = 'red', pch ='x', cex = 0.6)
+grid()
+abline(v=65, lty = 2, col = 'darkseagreen4', lwd=1)
+legend('topleft', "Baseline: Highest ID First\nAllocated IDs: 1-61,64, Reserved IDs: 62-63,65-200", pch = 'x', col = 'red', cex = 0.8)
+
+par(mar = c(4,4,0,1))
+plot(v$V1, xlim=c(1,200), ylim = c(0,1.5), ylab = 'Scheduling Time (s)', las = 1, xlab = "Job ID", col = 'blue', pch ='o', cex = 0.6)
+grid()
+abline(v=65, lty = 2, col = 'darkseagreen4', lwd=1)
+legend('topleft', "Variation Aware: Most Efficient Node First\nAllocated IDs: 1-61,64, Reserved IDs: 62-63,65-200", pch = 'o', col = 'blue', cex = 0.8)
+
+dev.off()
