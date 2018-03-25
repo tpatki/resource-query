16d74c9e92f4ac86b782dc353cb4d6e78ca38092
Tapasya Patki <patki1@llnl.gov>
2018-03-24 18:22:48 -0700

    Add results for lowest ID first

--- patki-tests/early_results/helper_cmds.sh
+++ patki-tests/early_results/helper_cmds.sh
@@ -1,11 +1,13 @@
-cat baseline_results.dat | grep ELAPSE | cut -d "=" -f 2 > baseline_sched_time
+cat baseline_low.dat | grep ELAPSE | cut -d "=" -f 2 > low_sched_time
+cat baseline_high.dat | grep ELAPSE | cut -d "=" -f 2 > high_sched_time
 cat varaware_results.dat | grep ELAPSE | cut -d "=" -f 2 > varaware_sched_time
-cat baseline_results.dat | grep RESOURCES | cut -d "=" -f 2 > baseline_alloc_vs_reserve
+cat baseline_low.dat | grep RESOURCES | cut -d "=" -f 2 > low_alloc_vs_reserve
+cat baseline_high.dat | grep RESOURCES | cut -d "=" -f 2 > high_alloc_vs_reserve
 cat varaware_results.dat | grep RESOURCES | cut -d "=" -f 2 > varaware_alloc_vs_reserve
 
 # The following did not show any difference for this test: same JOB IDs were allocated/reserved.
 # 62 jobs were allocated, rest were reserved for future time.
-diff --side-by-side varaware_alloc_vs_reserve baseline_alloc_vs_reserve
+diff --side-by-side varaware_alloc_vs_reserve low_alloc_vs_reserve
 
 cd ..
 cat cmd_inp_quartz | cut -d 'b' -f 4 | cut -d '_' -f 2 | cut -d '.' -f 1 > jobIDs
--- patki-tests/early_results/sched_time.R
+++ patki-tests/early_results/sched_time.R
@@ -1,14 +1,22 @@
-b<-read.table('baseline_sched_time', header = FALSE)
+b<-read.table('high_sched_time', header = FALSE)
+l<-read.table('low_sched_time', header = FALSE)
 v<-read.table('varaware_sched_time', header = FALSE)
 
-pdf("sched_time.pdf", width = 8, height = 6)
-par(mar = c(3,4,1,1), mfrow=c(2,1))
+pdf("sched_time.pdf", width = 8, height = 9)
+par(mar = c(3,4,1,1), mfrow=c(3,1))
 
 plot(b$V1, xlim=c(1,200), ylim = c(0,1.5), ylab = 'Scheduling Time (s)', las = 1, xlab = "", col = 'red', pch ='x', cex = 0.6)
 grid()
 abline(v=65, lty = 2, col = 'darkseagreen4', lwd=1)
 legend('topleft', "Baseline: Highest ID First\nAllocated IDs: 1-61,64, Reserved IDs: 62-63,65-200", pch = 'x', col = 'red', cex = 0.8)
 legend('topright', paste("Total: ",round(sum(b$V1),2), 's', sep=''), cex=0.8)
+
+par(mar = c(3,4,0,1))
+plot(l$V1, xlim=c(1,200), ylim = c(0,1.5), ylab = 'Scheduling Time (s)', las = 1, xlab = "", col = 'forestgreen', pch ='x', cex = 0.6)
+grid()
+abline(v=65, lty = 2, col = 'darkseagreen4', lwd=1)
+legend('topleft', "Baseline: Lowest ID First\nAllocated IDs: 1-61,64, Reserved IDs: 62-63,65-200", pch = 'o', col = 'forestgreen', cex = 0.8)
+legend('topright', paste("Total: ",round(sum(l$V1),2), 's', sep=''), cex=0.8)
 
 par(mar = c(4,4,0,1))
 plot(v$V1, xlim=c(1,200), ylim = c(0,1.5), ylab = 'Scheduling Time (s)', las = 1, xlab = "Job ID", col = 'blue', pch ='o', cex = 0.6)
